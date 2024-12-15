73b0b2f4982c84c802d6df7fd187d306f9bc0450
Jiang Sheng <jiangshengdev@outlook.com>
2024-12-15 19:02:08 +0800

    annotation

--- data_structures/_binary_search_node.ts
+++ data_structures/_binary_search_node.ts
@@ -3,12 +3,23 @@
 
 export type Direction = 'left' | 'right';
 
+/**
+ * 表示二叉搜索树中的一个节点。
+ *
+ * @typeparam T 存储在节点中的值的类型。
+ */
 export class BinarySearchNode<T> {
   left: BinarySearchNode<T> | null;
   right: BinarySearchNode<T> | null;
   parent: BinarySearchNode<T> | null;
   value: T;
 
+  /**
+   * 创建一个新的二叉搜索节点。
+   *
+   * @param parent 父节点。
+   * @param value 节点存储的值。
+   */
   constructor(parent: BinarySearchNode<T> | null, value: T) {
     this.left = null;
     this.right = null;
@@ -16,6 +27,12 @@ export class BinarySearchNode<T> {
     this.value = value;
   }
 
+  /**
+   * 从给定节点创建一个副本。
+   *
+   * @param node 要复制的节点。
+   * @returns 新创建的节点副本。
+   */
   static from<T>(node: BinarySearchNode<T>): BinarySearchNode<T> {
     const copy: BinarySearchNode<T> = new BinarySearchNode(
       node.parent,
@@ -26,6 +43,11 @@ export class BinarySearchNode<T> {
     return copy;
   }
 
+  /**
+   * 确定当前节点相对于其父节点的位置方向。
+   *
+   * @returns 如果当前节点是父节点的左子节点，返回 `'left'`；如果是右子节点，返回 `'right'`；否则返回 `null`。
+   */
   directionFromParent(): Direction | null {
     return this.parent === null
       ? null
@@ -36,18 +58,33 @@ export class BinarySearchNode<T> {
           : null;
   }
 
+  /**
+   * 查找当前节点的最小子节点。
+   *
+   * @returns 包含最小值的节点。
+   */
   findMinNode(): BinarySearchNode<T> {
     let minNode: BinarySearchNode<T> | null = this.left;
     while (minNode?.left) minNode = minNode.left;
     return minNode ?? this;
   }
 
+  /**
+   * 查找当前节点的最大子节点。
+   *
+   * @returns 包含最大值的节点。
+   */
   findMaxNode(): BinarySearchNode<T> {
     let maxNode: BinarySearchNode<T> | null = this.right;
     while (maxNode?.right) maxNode = maxNode.right;
     return maxNode ?? this;
   }
 
+  /**
+   * 查找当前节点的后继节点。
+   *
+   * @returns 后继节点，如果不存在则返回 `null`。
+   */
   findSuccessorNode(): BinarySearchNode<T> | null {
     if (this.right !== null) return this.right.findMinNode();
     let parent: BinarySearchNode<T> | null = this.parent;
--- data_structures/_red_black_node.ts
+++ data_structures/_red_black_node.ts
@@ -4,17 +4,34 @@
 import { BinarySearchNode, type Direction } from './_binary_search_node.ts';
 export type { Direction };
 
+/**
+ * 表示红黑树中的一个节点，继承自二叉搜索树的节点。
+ *
+ * @typeparam T 存储在节点中的值的类型。
+ */
 export class RedBlackNode<T> extends BinarySearchNode<T> {
   declare parent: RedBlackNode<T> | null;
   declare left: RedBlackNode<T> | null;
   declare right: RedBlackNode<T> | null;
   red: boolean;
 
+  /**
+   * 创建一个新的红黑节点。
+   *
+   * @param parent 节点的父节点。
+   * @param value 节点存储的值。
+   */
   constructor(parent: RedBlackNode<T> | null, value: T) {
     super(parent, value);
     this.red = true;
   }
 
+  /**
+   * 从现有的红黑节点创建一个副本。
+   *
+   * @param node 要复制的红黑节点。
+   * @returns 新创建的红黑节点副本。
+   */
   static override from<T>(node: RedBlackNode<T>): RedBlackNode<T> {
     const copy: RedBlackNode<T> = new RedBlackNode(node.parent, node.value);
     copy.left = node.left;
