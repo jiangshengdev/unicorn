cbb3e8353e9c0f62a884fca337f052c54889243b
Jiang Sheng <jiangshengdev@outlook.com>
2024-12-15 20:06:31 +0800

    annotation

--- data_structures/_binary_search_node.ts
+++ data_structures/_binary_search_node.ts
@@ -9,9 +9,13 @@ export type Direction = 'left' | 'right';
  * @typeparam T 存储在节点中的值的类型。
  */
 export class BinarySearchNode<T> {
+  // 左子节点，类型为 BinarySearchNode<T> 或 null
   left: BinarySearchNode<T> | null;
+  // 右子节点，类型为 BinarySearchNode<T> 或 null
   right: BinarySearchNode<T> | null;
+  // 父节点，类型为 BinarySearchNode<T> 或 null
   parent: BinarySearchNode<T> | null;
+  // 存储在节点中的值，类型为 T
   value: T;
 
   /**
@@ -47,6 +51,7 @@ export class BinarySearchNode<T> {
     copy.left = node.left;
     // 复制右子节点
     copy.right = node.right;
+    // 返回新创建的节点副本
     return copy;
   }
 
@@ -56,12 +61,25 @@ export class BinarySearchNode<T> {
    * @returns 如果当前节点是父节点的左子节点，返回 `'left'`；如果是右子节点，返回 `'right'`；否则返回 `null`。
    */
   directionFromParent(): Direction | null {
-    // 检查是否有父节点
-    if (this.parent === null) return null;
-    // 当前节点是父节点的左子节点
-    if (this === this.parent.left) return 'left';
-    // 当前节点是父节点的右子节点
-    return 'right';
+    // 如果当前节点没有父节点，返回 null
+    if (this.parent === null) {
+      return null;
+    } else {
+      // 如果当前节点是父节点的左子节点
+      if (this === this.parent.left) {
+        // 返回 'left'
+        return 'left';
+      } else {
+        // 如果当前节点是父节点的右子节点
+        if (this === this.parent.right) {
+          // 返回 'right'
+          return 'right';
+        } else {
+          // 如果既不是左子节点也不是右子节点，返回 null
+          return null;
+        }
+      }
+    }
   }
 
   /**
@@ -70,14 +88,14 @@ export class BinarySearchNode<T> {
    * @returns 包含最小值的节点。
    */
   findMinNode(): BinarySearchNode<T> {
-    // 从左子节点开始，寻找最小节点
+    // 从左子节点开始，假定最小节点为左子节点
     let minNode: BinarySearchNode<T> | null = this.left;
-    // 循环直到找到最左边的节点
+    // 循环查找最左下方的节点
     while (minNode?.left) {
-      // 继续向左查找
+      // 继续向左子节点遍历
       minNode = minNode.left;
     }
-    // 如果没有更左的节点，则返回当前节点
+    // 如果不存在左子节点，最小节点为当前节点
     return minNode ?? this;
   }
 
@@ -87,14 +105,14 @@ export class BinarySearchNode<T> {
    * @returns 包含最大值的节点。
    */
   findMaxNode(): BinarySearchNode<T> {
-    // 从右子节点开始，寻找最大节点
+    // 从右子节点开始，假定最大节点为右子节点
     let maxNode: BinarySearchNode<T> | null = this.right;
-    // 循环直到找到最右边的节点
+    // 循环查找最右下方的节点
     while (maxNode?.right) {
-      // 继续向右查找
+      // 继续向右子节点遍历
       maxNode = maxNode.right;
     }
-    // 如果没有更右的节点，则返回当前节点
+    // 如果不存在右子节点，最大节点为当前节点
     return maxNode ?? this;
   }
 
@@ -108,7 +126,9 @@ export class BinarySearchNode<T> {
     if (this.right !== null) return this.right.findMinNode();
     // 否则，向上遍历父节点，找到第一个当前节点是其左子节点的父节点
     let parent: BinarySearchNode<T> | null = this.parent;
+    // 获取当前节点相对于父节点的方向
     let direction: Direction | null = this.directionFromParent();
+    // 当父节点存在且当前节点是父节点的右子节点时
     while (parent && direction === 'right') {
       // 更新方向和父节点
       direction = parent.directionFromParent();
